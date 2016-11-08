2722d7315e15ba49513976fe66059bd161ccf42d
antoniofalcone89 <antonio.falcone89@gmail.com>
2016-11-08 12:24:22 +0100

    Bugfix + cookies + indexpage

--- js/functions.js
+++ js/functions.js
@@ -62,6 +62,9 @@ function myAjax (nomefile) {
             complete: function (data) {
                 setImg(nomeimg);
                 $('.waiting').hide();
+                //$("#heatmap1").replaceWith($("#heatmap1").clone());
+                $("#firstimg1").closest('.gruppoinput1').children('p.nomefile').remove();
+                $("#firstimg2").closest('.gruppoinput2').children('p.nomefile').remove();
             },
             beforeSend:function(){
                 $('.waiting').show();
@@ -70,6 +73,10 @@ function myAjax (nomefile) {
     }
     else{
         alert("Richiesto un file di tipo csv");
+        //$("#heatmap1").replaceWith($("#heatmap1").clone());
+        //$("#heatmap2").replaceWith($("#heatmap2").clone());
+        $("#firstimg1").closest('.gruppoinput1').children('p.nomefile').remove();
+        $("#firstimg2").closest('.gruppoinput2').children('p.nomefile').remove();
     }
 
 }
